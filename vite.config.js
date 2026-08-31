fa32af4b95d09ed47b8fbb3500232ab187293116
gaurav <gauravsinghsisodiya963@gmail.com>
2026-08-31 16:31:41 +0530

    feat: Update API configuration to use environment variable for base URL and remove proxy settings

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server : {
-    proxy : {
-      '/api' : "https://backend-gspi.onrender.com"
-    }
-  },
   plugins: [
     react(),
     tailwindcss(),
